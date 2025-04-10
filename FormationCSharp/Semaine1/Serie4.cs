161b9258170d213636834e2d31cbe014b746d5fa
Darren <stephandarren4@gmail.com>
2025-04-10 16:59:05 +0200

    debut serie 4 commit du soir 10/04

--- FormationCSharp/Semaine1/Serie4.cs
+++ FormationCSharp/Semaine1/Serie4.cs
@@ -10,14 +10,64 @@ using System.Threading.Tasks;
 
 namespace Semaine1
 {
-    internal class Serie2
+    internal class Serie4
     {
-        
+        /* reponses au question : 
+         * exercice 1 1. pour chaque code morse il faut pouvoir stocker la lettre correspondante 
+         *              une liste n'est pas pratique à utiliser dans cette situation car les valeurs 
+         *              sont recupérés à partir d'index avec un dictionnaire on peut recuperer une lettre
+         *              à partir de sa valeur morse en faisant dct[stringMorse] 
+         */
+
+
         static void Main(string[] args)
         {
-            
+            Dictionary<char, string> morseCode = new Dictionary<char, string>()
+            {
+                {'A', "=.==="},
+                {'B', "===.=.=.="},
+                {'C', "===.=.===.="},
+                {'D', "===.=.="},
+                {'E', "="},
+                {'F', "=.=.===.="},
+                {'G', "===.===.="},
+                {'H', "=.=.=.="},
+                {'I', "=.="},
+                {'J', "=.===.===.==="},
+                {'K', "===.=.==="},
+                {'L', "=.===.=.="},
+                {'M', "===.==="},
+                {'N', "===.="},
+                {'O', "===.===.==="},
+                {'P', "=.===.===.="},
+                {'Q', "===.===.=.==="},
+                {'R', "=.===.="},
+                {'S', "=.=.="},
+                {'T', "==="},
+                {'U', "=.=.==="},
+                {'V', "=.=.=.==="},
+                {'W', "=.===.==="},
+                {'X', "===.=.=.==="},
+                {'Y', "===.=.===.==="},
+                {'Z', "===.===.=.="}
+            };
+            // Inversion du dictionnaire : morse -> caractère
+            var reversedMorseCode = morseCode.ToDictionary(kvp => kvp.Value, kvp => kvp.Key);
 
+            string code = "=.=.=.=...=...=.===.=.=...=.===.=.=...===.===.===.....=.===.===...===.===.===...=.===.=...=.===.=.=...===.=.=";
+            Console.WriteLine(WordsCount(code));
+            
             Console.ReadKey();
+        }
+
+        public static int LettersCount(string code)
+        {
+            return 0;
+        }
+
+        public static int WordsCount(string code)
+        {
+
         }
     }
 }
